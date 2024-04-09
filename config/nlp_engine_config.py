2dcbf00e89fa8405c5727cac205179a0113801d5
Sowmya.L.R <sowmyalr@sahaj.ai>
2024-04-09 12:16:37 +0530

    adding nlp_engine_config, flair_recognizer as base setup

---
+++ config/nlp_engine_config.py
@@ -0,0 +1,28 @@
+from recognizer.flair_recognizer import FlairRecognizer
+from presidio_analyzer import RecognizerRegistry
+from presidio_analyzer.nlp_engine import NlpEngineProvider
+import spacy
+
+
+class NLPEngineConfig:
+    def create_nlp_engine(self, model_path):
+        pass
+
+
+class FlairNLPEngine(NLPEngineConfig):
+    def create_nlp_engine(self, model_path):
+        registry = RecognizerRegistry()
+        registry.load_predefined_recognizers()
+        if not spacy.util.is_package("en_core_web_sm"):
+            spacy.cli.download("en_core_web_sm")
+        flair_recognizer = FlairRecognizer(model_path=model_path)
+        nlp_configuration = {
+            "nlp_engine_name": "spacy",
+            "models": [{"lang_code": "en", "model_name": "en_core_web_sm"}],
+        }
+        registry.add_recognizer(flair_recognizer)
+        registry.remove_recognizer("SpacyRecognizer")
+
+        nlp_engine = NlpEngineProvider(nlp_configuration=nlp_configuration).create_engine()
+
+        return nlp_engine, registry
