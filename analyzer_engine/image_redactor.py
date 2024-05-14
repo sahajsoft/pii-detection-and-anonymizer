e2092c3ba1f2e57daa6f1cec97487387eb86658d
Sowmya.L.R <sowmyalr@sahaj.ai>
2024-05-14 14:15:05 +0530

    Image redactor ImageUtil performs local file operations. This can be extended for cloud file operations too Modified pyproject for including the dependencies

---
+++ analyzer_engine/image_redactor.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from presidio_image_redactor import ImageRedactorEngine
+
+from util.image_util import ImageUtil
+
+
+class ImageRedactor:
+    def __init__(self, image_file):
+        self.engine = ImageRedactorEngine()
+        self.image_file = image_file
+
+    def detect_pii(self):
+        image = ImageUtil.open_image(self.image_file)
+        redacted_image = self.engine.redact(image, (255, 192, 203))
---
+++ util/image_util.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+
+class ImageUtil:
+    def save_image(self):
+        pass
+
+    @staticmethod
+    def open_image(image_name):
+        try:
+            img = Image.open(image_name)
+            return img
+        except Exception as e:
+            print(f"Error opening image: {e}")
+            return None
