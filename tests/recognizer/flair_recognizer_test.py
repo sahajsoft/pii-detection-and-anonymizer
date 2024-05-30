9c1a6f5d7d5f1efb3f34add0a6c31227ef801943
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2024-05-30 10:20:43 +0100

    migrate to pytest

--- tests/recognizer/flair_recognizer_test.py
+++ tests/recognizer/flair_recognizer_test.py
@@ -1,20 +1,17 @@
-import unittest
+import pytest
 
 from recognizer.flair_recognizer import FlairRecognizer
 
 
-class TestFlairRecognizer(unittest.TestCase):
-    def setUp(self) -> None:
-        self.recognizer = FlairRecognizer(model_path="flair/ner-english-large")
+def test_flair_recognizer_analyze():
+    recognizer = FlairRecognizer(model_path="flair/ner-english-large")
+    test_data = "Sowmya is working in Berkley bank as an accountant since 2021"
+    result = recognizer.analyze(test_data)
+    assert len(result) > 0
 
-    def test_flair_recognizer_analyse(self):
-        test_data = "Sowmya is working in Berkley bank as an accountant since 2021"
-        result = self.recognizer.analyze(test_data)
-        self.assertGreater(len(result), 0)
-
-    def test_flair_recognizes_persons_correctly(self):
-        test_data = "Sowmya is a person name"
-        self.assertGreater(len(self.recognizer.analyze(test_data)), 0)
-        test_data = "XXXXXX is a valid name?"
-        self.assertEquals(len(self.recognizer.analyze(test_data)), 0)
-
+def test_flair_recognizes_persons_correctly():
+    recognizer = FlairRecognizer(model_path="flair/ner-english-large")
+    test_data = "Sowmya is a person name"
+    assert len(recognizer.analyze(test_data)) > 0
+    test_data = "XXXXXX is a valid name?"
+    assert len(recognizer.analyze(test_data)) == 0
