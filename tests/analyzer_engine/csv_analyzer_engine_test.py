ba7c2b2c984aa839b911095e7373378e4b648d89
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2024-05-31 11:37:31 +0100

    fix sample_data.csv path for broken test

--- tests/analyzer_engine/csv_analyzer_engine_test.py
+++ tests/analyzer_engine/csv_analyzer_engine_test.py
@@ -8,7 +8,7 @@ def test_csv_analyzer_engine_anonymizer():
     nlp_engine = FlairNLPEngine("flair/ner-english-large")
     csv_analyzer = CSVAnalyzerEngine(nlp_engine)
     from presidio_anonymizer import BatchAnonymizerEngine
-    analyzer_results = csv_analyzer.analyze_csv('../sample_data/sample_data.csv', language="en")
+    analyzer_results = csv_analyzer.analyze_csv('./tests/sample_data/sample_data.csv', language="en")
 
     anonymizer = BatchAnonymizerEngine()
     anonymized_results = anonymizer.anonymize_dict(analyzer_results)
