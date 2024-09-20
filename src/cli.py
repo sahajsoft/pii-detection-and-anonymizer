54361ff2e5373133bb5e3dd885503309f32c8cce
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2024-09-20 16:04:36 +0100

    add vault to cli

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,11 @@
 import argparse
+
+from presidio_anonymizer.entities.engine.result.operator_result import OperatorResult
 from analyzer_engine.csv_analyzer_engine import CSVAnalyzerEngine
-from text.text import text_analyzer, text_anonymizer
 from presidio_anonymizer import BatchAnonymizerEngine
 from config.nlp_engine_config import FlairNLPEngine
+from operators.vault import Vault
+from text.text import text_analyzer, text_anonymizer
 
 NLP_ENGINE = "flair/ner-english-large"
 
@@ -30,13 +33,30 @@ def anonymize(args):
     anonymized_results = None
 
     if args.text:
-        anonymized_results = text_anonymizer(args.text, analyzer_results)
+        if args.vaulturl:
+            vault = Vault(args.vaulturl, args.vaultkey, args.vaulttoken)
+            anonymized_results = vault.anonymize(args.text, analyzer_results)
+        else:
+            anonymized_results = text_anonymizer(args.text, analyzer_results)
     else:
         anonymizer = BatchAnonymizerEngine()
         anonymized_results = anonymizer.anonymize_dict(analyzer_results)
 
     print(anonymized_results)
     return anonymized_results
+
+
+def deanonymize(args):
+    vault = Vault(args.vaulturl, args.vaultkey, args.vaulttoken)
+    anonymized_results = []
+    import json
+    for r in json.loads(args.anonymized_results_list):
+        anonymized_results.append(OperatorResult(r['start'], r['end'], r['entity_type'], r['text'], r['operator']))
+
+    deanonymized_result = vault.deanonymize(args.text, anonymized_results)
+
+    print(deanonymized_result)
+    return deanonymized_result
 
 
 def main():
@@ -53,7 +73,18 @@ def main():
     anonymizer_parser.add_argument("--filepath", required=False, type=str, metavar="FILE")
     anonymizer_parser.add_argument("--text", required=False, type=str)
     anonymizer_parser.add_argument("--language", required=False, type=str, default="en")
+    anonymizer_parser.add_argument("--vaulturl", required=False, type=str)
+    anonymizer_parser.add_argument("--vaulttoken", required=False, type=str)
+    anonymizer_parser.add_argument("--vaultkey", required=False, type=str)
     anonymizer_parser.set_defaults(func=anonymize)
+
+    deanonymizer_parser = subparsers.add_parser("deanonymize", description="Deanonymize inputs")
+    deanonymizer_parser.add_argument("--text", required=True, type=str)
+    deanonymizer_parser.add_argument("--vaulturl", required=True, type=str)
+    deanonymizer_parser.add_argument("--vaulttoken", required=False, type=str)
+    deanonymizer_parser.add_argument("--vaultkey", required=True, type=str)
+    deanonymizer_parser.add_argument("--anonymized_results_list", required=True, type=str)
+    deanonymizer_parser.set_defaults(func=deanonymize)
 
     args = parser.parse_args()
     args.func(args)
@@ -79,6 +110,7 @@ if __name__ == '__main__':
 # print("Anonymize:")
 # anon_res = vault.anonymize(t, res)
 # print(anon_res.text)
+# print(anon_res.items)
 
 # print("Deanonymize:")
 # deanon_res = vault.deanonymize(anon_res.text, anon_res.items)
